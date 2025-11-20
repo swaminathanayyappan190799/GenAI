a87421b369a07b04fec4a2ff442d3a15d3841d0f
swaminathanayyappan190799 <swamynathanayyappan@gmail.com>
2025-11-20 19:52:42 +0000

    ENH: Added code for few shot prompt templates

---
+++ langchain/few_shot_templates.py
@@ -0,0 +1,36 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
+
+
+llm = OllamaLLM(
+     model="gemma3:latest",
+     verbose=True,
+     temperature=0.6,
+     seed=13
+)
+
+examples = [
+    {"word": "bright", "opposite": "dark"},
+    {"word": "day", "opposite": "night"}
+]
+
+prompt = FewShotPromptTemplate(
+    input_variables=["word"],
+    example_prompt=PromptTemplate(
+        input_variables=["word", "opposite"],
+        template="{word} -> {opposite}",
+    ),
+    examples=examples,
+    prefix=(
+        "You are provided with a few examples of words and their antonyms.\n"
+        "Use the examples to determine the antonym for the user's input.\n\n"
+        "Examples:"
+    ),
+    suffix="\nNow provide the antonym for: {word}"
+)
+
+chain = prompt | llm
+
+results = chain.invoke("sad")
+
+print(results)
